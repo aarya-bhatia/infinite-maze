944cf59fc105d234deceee2444047d40adb6ce4d
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2022-04-12 14:44:30 -0500

    use mongodb and refactor

--- app.py
+++ app.py
@@ -1,40 +1,13 @@
-from datetime import datetime
 from flask import Flask, render_template
+from pymongo import MongoClient
 import requests
 
-mg_db = [
-    {
-        "id": "1",
-        "url": "http://127.0.0.1:24000/",
-        "last_access": None,
-        "status": "available", # possible values are available, waiting, error
-        "accept_size": "*:*"
-    }
-]
-
-retry_time = 5*60 #seconds
+mongo = MongoClient('localhost', 27017)
+db = mongo["cs240-project-maze"]
+mgdb = db['mg']
 
 app = Flask(__name__)
 
-def is_valid_mg(mg):
-    if "last_access" not in mg or mg["last_access"] is None:
-        mg["last_access"] = datetime.now().isoformat()
-
-    last_access = datetime.fromisoformat(mg["last_access"])
-    status = mg["status"]
-
-    if status == "error":
-        return False
-
-    if status == "waiting":
-        delta = datetime.now() - last_access
-
-        if delta.seconds >= retry_time:
-           mg["status"] = "available"
-        else:
-            return False
-    
-    return True
 
 def validate_grid(num_rows, num_cols, grid):
     if len(grid) != int(num_rows):
@@ -48,9 +21,10 @@ def validate_grid(num_rows, num_cols, grid):
 
     return True
 
+
 def has_available_size(num_rows, num_cols, mg):
     accept_size = mg["accept_size"].split(",")
-    
+
     for size in accept_size:
         dimensions = size.split(":")
         if (dimensions[0] == "*" or dimensions[0] == num_rows) and (dimensions[1] == '*' or dimensions[1] == num_cols):
@@ -70,40 +44,29 @@ def GET_index():
 
 @app.route('/generateSegment', methods=["GET"])
 def GET_maze_segment():
-    print(mg_db)
-
     num_rows = '7'
     num_cols = '7'
 
-    for mg in mg_db:
-        url = mg["url"]
+    docs = mgdb.find({"status": "available"})
 
-        if not is_valid_mg(mg) or mg["status"] != "available":
-            continue 
+    for mg in docs:
+        url = mg["URL"]
 
         if not has_available_size(num_rows, num_cols, mg):
             continue
-
-        mg["last_access"] = datetime.now().isoformat()
 
         try:
             response = requests.get(url)
 
             if response.status_code == 200:
                 response = response.json()
-                
+
                 if "geom" in response:
                     grid = response["geom"]
 
                     if validate_grid(num_rows, num_cols, grid):
                         return {"geom": response["geom"]}
+        except:
+            mg.update_one({"_id": mg["_id"]}, {"$set": {"status", "error"}})
 
-        except requests.exceptions.RequestException as e:
-            raise SystemExit(e)
-
-        mg["status"] = "waitig"
-    
-    # end for loop;
-
-    # no available servers
-    return {"error": "Internal server error"}, 500
+    return {"error": "No servers are available"}, 500
